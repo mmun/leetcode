1bf1b16c6388ed5f64511ad72a9c7590be921dca
Martin Muñoz <im.mmun@gmail.com>
2019-10-24 12:47:28 -0400

    27

---
+++ src/27.remove-element.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n-1]
+                n -= 1
+            else:
+                i += 1
+        return n
+                
+# @lc code=end
+
